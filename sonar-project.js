1435e758203a795b2f78e28d5648b00a08dd2ea7
santoshvivan <panchal.santosh1986@gmail.com>
2024-07-06 18:06:22 +0530

    Update sonar-project.js in master branch

--- sonar-project.js
+++ sonar-project.js
@@ -1,14 +1,14 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.40.109.48:9000/',
+  serverUrl: 'http://13.201.89.179:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
 	    'sonar.login': 'squ_4f95478fe616d8f91577c00fb18deed499888d60',
-	    //'sonar.login': 'admin',
-	    //'sonar.password': 'admin',
+	    'sonar.login': 'admin',
+	    'sonar.password': 'admin@12345',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
